64d5b1a0088d8b8dafe324d1d445528ab24f22f5
ShaoMun <hoshaomun0479@gmail.com>
2024-10-30 05:01:24 +0800

    leng leng

--- components/trophy.js
+++ components/trophy.js
@@ -24,7 +24,9 @@ const Trophy = () => {
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x000000, 0); // Transparent background
-    mountRef.current.appendChild(renderer.domElement);
+    if (mountRef.current) {
+      mountRef.current.appendChild(renderer.domElement);
+    }
     rendererRef.current = renderer;
 
     // Lighting
@@ -67,14 +69,14 @@ const Trophy = () => {
 
       // Stem
       const stemGeometry = new THREE.CylinderGeometry(0.2, 0.4, 2, 32);
-      const stemMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.3, roughness: 0.05 });
+      const stemMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.6, roughness: 0.05 });
       const stem = new THREE.Mesh(stemGeometry, stemMaterial);
       stem.position.y = 1.5;
       group.add(stem);
 
       // Inverted bowl
       const invertedBowlGeometry = new THREE.SphereGeometry(1.5, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2);
-      const invertedBowlMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.3, roughness: 0.05, side: THREE.DoubleSide });
+      const invertedBowlMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.6, roughness: 0.05, side: THREE.DoubleSide });
       const invertedBowl = new THREE.Mesh(invertedBowlGeometry, invertedBowlMaterial);
       invertedBowl.rotation.x = Math.PI;
       invertedBowl.scale.y = 1.4;
@@ -83,7 +85,7 @@ const Trophy = () => {
 
       // Handles
       const handleGeometry = new THREE.TorusGeometry(0.7, 0.11, 16, 32, Math.PI);
-      const handleMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.3, roughness: 0.05 });
+      const handleMaterial = new THREE.MeshStandardMaterial({ color: 0xFFFF00, metalness: 0.6, roughness: 0.05 });
 
       const leftHandle = new THREE.Mesh(handleGeometry, handleMaterial);
       leftHandle.position.set(-1, 2.7, 0);
@@ -145,8 +147,10 @@ const Trophy = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', handleResize);
-      if (rendererRef.current) {
+      if (mountRef.current && rendererRef.current) {
         mountRef.current.removeChild(rendererRef.current.domElement);
+      }
+      if (rendererRef.current) {
         rendererRef.current.dispose();
       }
       cancelAnimationFrame(frameIdRef.current);
@@ -158,7 +162,7 @@ const Trophy = () => {
       ref={mountRef}
       style={{
         position: 'fixed',
-        top: '10%',
+        top: '0',
         left: '0',
         width: '100%',
         height: '100%',
