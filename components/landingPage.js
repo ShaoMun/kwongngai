153931b0c0a9afca83c18ea392b257e4fcea2154
ShaoMun <hoshaomun0479@gmail.com>
2024-10-30 17:11:17 +0800

    last commit

--- components/landingPage.js
+++ components/landingPage.js
@@ -189,7 +189,7 @@ const LandingPage = () => {
         <h3>The Grand Ho Tram</h3>
         <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
       <iframe 
-        src="https://player.vimeo.com/video/1024647507?autoplay=1&muted=1&title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479" 
+        src="https://player.vimeo.com/video/1024647507?autoplay=1&muted=1&loop=1&title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479" 
         frameBorder="0" 
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
